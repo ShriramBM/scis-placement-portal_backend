96f5b72a65f130b17d18ea55f92be891108ce146
Vijay Singh Parihar <vsp.krashi@gmail.com>
2026-02-24 00:11:25 +0530

    pc login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -185,3 +185,54 @@ export const login = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+
+export const placementCoordinatorLogin = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    if (user.role !== "PLACEMENT_COORDINATOR") {
+      return res.status(403).json({
+        message: "Access denied. Not a Placement Coordinator",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Login failed",
+    });
+  }
+};
